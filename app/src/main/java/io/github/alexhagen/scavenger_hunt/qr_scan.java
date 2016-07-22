62d5457b2d98fb840046b3babd7f24f309a10329
Alex Hagen <ahagen@purdue.edu>
2016-07-22 06:31:12 -0400

    Initial commit

---
+++ app/src/main/java/io/github/alexhagen/scavenger_hunt/qr_scan.java
@@ -0,0 +1,13 @@
+package io.github.alexhagen.scavenger_hunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class qr_scan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scan);
+    }
+}
