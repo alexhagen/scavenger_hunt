e925dcf70b318ee0fb25b3d0fb94bfbd91b8922c
Alex Hagen <ahagen@purdue.edu>
2016-07-31 20:42:01 -0400

    got the location working, now to get the bearing/heading working.

--- app/src/main/java/io/github/alexhagen/scavenger_hunt/external_clue.java
+++ app/src/main/java/io/github/alexhagen/scavenger_hunt/external_clue.java
@@ -1,53 +1,142 @@
 package io.github.alexhagen.scavenger_hunt;
 
-
-
-import android.app.Dialog;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
+import android.content.IntentSender;
+import android.hardware.GeomagneticField;
 import android.location.Location;
-import android.location.LocationManager;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class external_clue extends FragmentActivity {
+
+public class external_clue extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG = external_clue.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationRequest mLocationRequest;
+    private GoogleMap mMap;
+    GeomagneticField geoField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_external_clue);
-
-        // TODO: get current position
-        // TODO: get current heading
-        // TODO: remove any touch callbacks so you cant move map
-        // TODO: measure distance from given coordinates
-        // TODO: measure heading to given coordinates
-        // TODO: plot current position, current heading, heading to given coords, and distance to given coords on map
-
-        /*
-        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = service.getBestProvider(criteria, false);
-        Location location = service.getLastKnownLocation(provider);
-        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());*/
-        Log.d("MAPPING", "hi");
-        double latitude = 0;
-        double longitude = 0;
-        gps_tracker tracker = new gps_tracker(this);
-        if (!tracker.canGetLocation()) {
-            tracker.showSettingsAlert();
-        } else {
-            latitude = tracker.getLatitude();
-            longitude = tracker.getLongitude();
-        }
-        Log.d("MAPPING", String.format("%f N, %f W", latitude, longitude));
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
     }
 
-    // TODO: update plot on positionChange
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setUpMap();
+    }
 
+    @Override
+    public void onConnected(Bundle bundle){
+        Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } else {
+            handleNewLocation(location);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i){
+        Log.i(TAG, "location services suspended.");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void handleNewLocation(Location location){
+        float heading ;
+        float bearing;
+
+        Log.d(TAG, location.toString());
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        geoField = new GeomagneticField(
+                Double.valueOf(location.getLatitude()).floatValue(),
+                Double.valueOf(location.getLongitude()).floatValue(),
+                Double.valueOf(location.getAltitude()).floatValue(),
+                System.currentTimeMillis()
+        );
+
+        heading = location.getBearing() + geoField.getDeclination();
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    private void setUpMap() {
+        mMap.getUiSettings().setScrollGesturesEnabled(false);
+        mMap.getUiSettings().setZoomGesturesEnabled(false);
+    }
 }
